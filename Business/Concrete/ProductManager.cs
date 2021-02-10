60cf7ac2737314731d1e0623557c8c3536fc8ab7
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-02-10 23:02:21 +0300

    Değer döndüren metotlar için IDataResult yapısı oluşturulmaya başlandı.

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -4,6 +4,7 @@ using Entities.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Business.Constants;
 using Core.Utilities.Results;
 using Entities.DTOs;
 
@@ -16,42 +17,43 @@ namespace Business.Concrete
         {
             _productDal = productDal;
         }
-        public List<Product> GetAll()
+        public IDataResult<List<Product>> GetAll()
         {
             //İş Kodları.
             //Yetkisi var mı ?
-            return _productDal.GetAll();
+            return new DataResult(_productDal.GetAll());
         }
 
-        public List<Product> GetAllByCategoryId(int id)
+        public IDataResult<List<Product>> GetAllByCategoryId(int id)
         {
             return _productDal.GetAll(p => p.CategoryId == id);
         }
 
-        public List<Product> GetByUnitPrice(decimal min, decimal max)
+        public IDataResult<List<Product>> GetByUnitPrice(decimal min, decimal max)
         {
             return _productDal.GetAll(p => p.UnitPrice >= min && p.UnitPrice <= max);
         }
 
-        public List<ProductDetailDto> GetProductDetails()
+        public IDataResult<List<ProductDetailDto>> GetProductDetails()
         {
             return _productDal.GetProductDetails();
         }
 
-        public Product GetById(int productId)
+        public IDataResult<Product> GetById(int productId)
         {
             return _productDal.Get(p => p.ProductId == productId);
         }
 
         public IResult Add(Product product)
         {
-            if (product.ProductName.Length<2)
+            if (product.ProductName.Length < 2)
             {
-                return new ErrorResult("Ürün ismi min 2 karakter olmalıdır.");
+                //magicstrings
+                return new ErrorResult(Messages.ProductNameInvalid);
             }
             //business codes
             _productDal.Add(product);
-            return new Result(true, "Ürün Eklendi.");
+            return new SuccessResult(Messages.ProductAdded);
         }
     }
 }
