5c1536f7d5eb8f2de757550505ebf52ae0421ebe
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-03 21:56:52 +0300

    DependencyResolvers Core katmanına taşındı.

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -12,6 +12,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Business.Abstract;
 using Business.Concrete;
+using Core.DependencyResolvers;
+using Core.Utilities.Extensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encryption;
 using Core.Utilities.Security.JWT;
@@ -57,7 +59,12 @@ namespace WebAPI
                         IssuerSigningKey = SecurityKeyHelper.CreateSecurityKey(tokenOptions.SecurityKey)
                     };
                 });
-            ServiceTool.Create(services);
+           // ServiceTool.Create(services);
+
+           services.AddDependencyResolvers(new ICoreModule[]
+           {
+               new CoreModule()
+           });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
