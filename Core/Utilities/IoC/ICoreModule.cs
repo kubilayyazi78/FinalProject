66ec237e6123761aa41e68f9417f559ba5849f9a
kubilayyazi78 <kubilayyazi@hotmail.com>
2021-03-03 23:08:38 +0300

    Cache alt yapısı oluşturuluyor.

--- Core/Utilities/IoC/ICoreModule.cs
+++ Core/Utilities/IoC/ICoreModule.cs
@@ -6,7 +6,7 @@ using Microsoft.Extensions.DependencyInjection;
 namespace Core.Utilities.IoC
 {
     public interface ICoreModule
-    {
+    {        
         void Load(IServiceCollection serviceCollection);
     }
 }
